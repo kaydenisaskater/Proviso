2da3158aff79e309588ea60c677342c5eeb8b9e7
Kayden Linner <linner.kayden@outlook.com>
2022-02-08 17:57:16 -0600

    Updating Dev Branch
    Updating Dev Branch for other developers to access.

--- src/main/java/controller/ProvisoServlet.java
+++ src/main/java/controller/ProvisoServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ProvisoServlet
  */
-@WebServlet("/ProvisoServlet")
+@WebServlet(name = "ProvisoServlet", urlPatterns = "/proviso/*")
 public class ProvisoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +27,7 @@ public class ProvisoServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	/**
@@ -35,7 +35,25 @@ public class ProvisoServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String base = "/jsp/";
+		String url = base + "index.jsp";
+		String action = request.getParameter("action");
+		
+		System.out.println("Action: " + action + "/n URL: " + url);
+		
+		if (action != null) {
+			switch (action) {
+				case "homepage":
+					url = base + "index.jsp";
+					break;
+				case "viewLogin":
+					url = base + "login.jsp";
+					break;
+			}
+		}
+		
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(url);
+		rd.forward(request, response);
 	}
 
 }
