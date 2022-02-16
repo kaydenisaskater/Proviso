173e6ee5646d12a8dbd9fae9362e0696905095f6
I12B-Semaj <smithjamesm18@gmail.com>
2022-02-15 23:51:54 -0600

    Fully Populated JDBC DAOs

--- src/main/java/model/JdbcReservationDao.java
+++ src/main/java/model/JdbcReservationDao.java
@@ -31,8 +31,8 @@ public class JdbcReservationDao implements ReservationDao
 			try 
 			{
 				Statement stmt = conn.createStatement(); 
-				String sql = String.format("INSERT INTO `proviso`.`reservations` (`check_in_date`, `check_out_date`, "
-						+ "`total_price`, `loyalty_points`, `room_size_id`, `guest_option_id`, `user_id`) "
+				String sql = String.format("INSERT INTO reservations (check_in_date, check_out_date, "
+						+ "total_price, loyalty_points, room_size_id, guest_option_id, user_id) "
 						+ "VALUES('%s', '%s', %s, %s, %s, %s, %s);", 
 						newReservation.getCheckIn(), newReservation.getCheckOut(), newReservation.getTotalPrice(),
 						newReservation.getLoyaltyPoints(), newReservation.getRoomSizeID(), newReservation.getGuestOptionID(),
@@ -70,7 +70,7 @@ public class JdbcReservationDao implements ReservationDao
 				Statement stmt = conn.createStatement();
 				String sql = "SELECT reservation_id, check_in_date, check_out_date, total_price, loyalty_points, "
 						+ " room_size_id, guest_option_id, user_id "
-						+ "FROM users";
+						+ "FROM reservations";
 				
 				try 
 				{
@@ -115,22 +115,129 @@ public class JdbcReservationDao implements ReservationDao
 		
 		return reservations;
 	}
-
-	@Override
-	public Reservation find(Long reservationID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void update(Reservation entity) {
-		// TODO Auto-generated method stub
+	
+	//Method to retrieve a list of all reservations with user_id
+	public List<Reservation> list(Long userID) 
+	{
+		Connection conn = db.getConn();
+		ArrayList<Reservation> reservations = new ArrayList<Reservation>();
 		
+		if(conn != null) 
+		{
+			try 
+			{
+				Statement stmt = conn.createStatement();
+				String sql = "SELECT reservation_id, check_in_date, check_out_date, total_price, loyalty_points, "
+						+ " room_size_id, guest_option_id, user_id "
+						+ "FROM reservations "
+						+ "WHERE user_id = " + userID;
+				
+				try 
+				{
+					ResultSet rs = stmt.executeQuery(sql);
+					
+					try 
+					{
+						while(rs.next()) 
+						{
+							Reservation reservation = new Reservation();
+							reservation.setReservationID(rs.getLong(1));
+							reservation.setCheckIn(rs.getString(2));
+							reservation.setCheckOut(rs.getString(3));
+							reservation.setTotalPrice(rs.getDouble(4));
+							reservation.setLoyaltyPoints(rs.getLong(5));
+							reservation.setRoomSizeID(rs.getLong(6));
+							reservation.setGuestOptionID(rs.getLong(7));
+							reservation.setUserID(rs.getLong(8));
+							reservations.add(reservation);
+						}
+					}
+					finally 
+					{
+						rs.close();
+					}
+				}
+				finally 
+				{
+					stmt.close();
+				}
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Could not retrieve user's reservations.");
+				System.out.println("SQL Exception: " + ex.getMessage());
+			}
+			finally 
+			{
+				db.closeConn(conn);
+			}
+		}
+		
+		return reservations;
 	}
-
+	
+	//Method to retrieve a reservation with reservation_id
 	@Override
-	public void remove(Long key) {
-		// TODO Auto-generated method stub
+	public Reservation find(Long reservationID) 
+	{
+		Connection conn = db.getConn(); 
+		
+		Reservation reservation = null;
+		
+		if (conn != null) 
+		{
+			try 
+			{
+				Statement stmt = conn.createStatement();
+				String sql = "SELECT reservation_id, check_in_date, check_out_date, total_price, loyalty_points, "
+						+ " room_size_id, guest_option_id, user_id "
+						+ "FROM reservations "
+						+ "WHERE reservation_id = " + reservationID;
+				
+				try 
+				{
+					ResultSet rs = stmt.executeQuery(sql);
+					
+					try 
+					{
+						if (rs.next()) 
+						{
+							reservation = new Reservation(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getDouble(4), 
+									rs.getLong(5), rs.getLong(6), rs.getLong(7), rs.getLong(8));
+						}
+					}
+					finally
+					{
+						rs.close();
+					}
+				}
+				finally 
+				{
+					stmt.close();
+				}				
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Could not retrieve reservation.");
+				System.out.println("SQL Exception: " + ex.getMessage());
+			}
+		}
+		
+		return reservation;
+	}
+	
+	//Method to update reservation record with reservation_id
+	@Override
+	public void update(Reservation entity) 
+	{
+		// TODO - No feature to update reservation yet.
+	}
+	
+	//Method to delete reservation record with reservation_id
+	@Override
+	public void remove(Long key) 
+	{
+		// TODO - No feature to delete reservation yet.
 		
 	}
 
--- src/main/java/model/JdbcRoomSizeDao.java
+++ src/main/java/model/JdbcRoomSizeDao.java
@@ -11,35 +11,216 @@ import beans.RoomSize;
 
 public class JdbcRoomSizeDao implements RoomSizeDao
 {
-
+	JdbcManager db = null;  
+	
+	public JdbcRoomSizeDao() 
+	{
+		db = new JdbcManager();
+	}
+	
+	//Method to add new room_size record to database
 	@Override
-	public void add(RoomSize entity) {
-		// TODO Auto-generated method stub
+	public void add(RoomSize entity) 
+	{
+		Connection conn = db.getConn(); 
+		RoomSize newRoomSize = entity;
 		
-	}
+		//Perform SQL INSERT INTO room_sizes Table and output success or fail
+		if(conn != null) 
+		{
+			try 
+			{
+				Statement stmt = conn.createStatement(); 
+				String sql = String.format("INSERT INTO room_sizes (room_size_description) VALUES('%s');", 
+						newRoomSize.getRoomSizeDescription());
 
-	@Override
-	public List<RoomSize> list() {
-		// TODO Auto-generated method stub
-		return null;
+				
+				try
+				{
+					stmt.executeUpdate(sql);
+					System.out.println("Inserted newRoomSize: " + newRoomSize.toString());
+				}
+				finally 
+				{
+					stmt.close();				
+				}
+			}
+			catch(SQLException ex)
+			{
+				System.out.println("Unable to insert newRoomSize: " + newRoomSize.toString());
+				System.out.println("SQL Exception: " + ex.getMessage());
+			}
+		}
 	}
-
+	
+	//Method to retrieve a list of all room_sizes
 	@Override
-	public RoomSize find(Long key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void update(RoomSize entity) {
-		// TODO Auto-generated method stub
+	public List<RoomSize> list() 
+	{
+		Connection conn = db.getConn();
+		ArrayList<RoomSize> roomSizes = new ArrayList<RoomSize>();
 		
-	}
-
-	@Override
-	public void remove(Long key) {
-		// TODO Auto-generated method stub
+		if(conn != null) 
+		{
+			try 
+			{
+				Statement stmt = conn.createStatement();
+				String sql = "SELECT room_size_id, room_size_description "
+						+ "FROM room_sizes";
+				
+				try 
+				{
+					ResultSet rs = stmt.executeQuery(sql);
+					
+					try 
+					{
+						while(rs.next()) 
+						{
+							RoomSize roomSize = new RoomSize();
+							roomSize.setRoomSizeID(rs.getLong(1));
+							roomSize.setRoomSizeDescription(rs.getString(2));
+							roomSizes.add(roomSize);
+						}
+					}
+					finally 
+					{
+						rs.close();
+					}
+				}
+				finally 
+				{
+					stmt.close();
+				}
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Could not retrieve room_sizes.");
+				System.out.println("SQL Exception: " + ex.getMessage());
+			}
+			finally 
+			{
+				db.closeConn(conn);
+			}
+		}
 		
+		return roomSizes;
 	}
-
+	
+	//Method to retrieve a room_size with room_size_id
+	@Override
+	public RoomSize find(Long key) 
+	{
+		Connection conn = db.getConn(); 
+		
+		RoomSize roomSize = null;
+		
+		if (conn != null) 
+		{
+			try 
+			{
+				Statement stmt = conn.createStatement(); 
+				String sql = "SELECT room_size_id, room_size_description FROM room_sizes "
+						+ "WHERE room_size_id = " + key;
+				
+				try 
+				{
+					ResultSet rs = stmt.executeQuery(sql);
+					
+					try 
+					{
+						if (rs.next()) 
+						{
+							roomSize = new RoomSize(rs.getLong(1), rs.getString(2));
+						}
+					}
+					finally
+					{
+						rs.close();
+					}
+				}
+				finally 
+				{
+					stmt.close();
+				}				
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Could not retrieve room_size.");
+				System.out.println("SQL Exception: " + ex.getMessage());
+			}
+		}
+		
+		return roomSize;
+	}
+	
+	//Method to update room_size record with room_size_id
+	@Override
+	public void update(RoomSize entity) 
+	{
+		Connection conn = db.getConn();
+		
+		if (conn != null) 
+		{
+			try 
+			{
+				Statement stmt = conn.createStatement();
+				String sql = "UPDATE room_sizes SET room_size_description = '" + entity.getRoomSizeDescription()
+					+ "', WHERE room_size_id = " + entity.getRoomSizeID();
+				
+				try 
+				{
+					stmt.executeUpdate(sql);
+					System.out.println("Updated room_size: " + entity.toString());
+				}
+				finally
+				{
+					stmt.close();
+				}
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Could not update room_size.");
+				System.out.println("SQL Exception: " + ex.getMessage());
+			}
+			finally 
+			{
+				db.closeConn(conn);
+			}
+		}
+	}
+	
+	//Method to delete room_size record with room_size_id
+	@Override
+	public void remove(Long key) 
+	{
+		Connection conn = db.getConn(); 
+		
+		if (conn != null) 
+		{
+			try
+			{
+				Statement stmt = conn.createStatement(); 
+				String sql = "DELETE FROM room_sizes WHERE room_size_id = " + key;
+				
+				try 
+				{
+					stmt.executeUpdate(sql);
+					System.out.println("Deleted room_size: roomSizeID = " + key);
+				}
+				finally
+				{
+					stmt.close();
+				}
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Could not delete room_size.");
+				System.out.println("SQL Exception: " + ex.getMessage());
+			}
+			finally 
+			{
+				db.closeConn(conn);
+			}
+		}
+	}
 }
